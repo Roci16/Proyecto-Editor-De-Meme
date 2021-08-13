93ec44627a6fcf82661235a1cad8c2c187743c41
Roci <ibrocio16@gmail.com>
2021-08-13 14:06:03 -0300

    Se agrega funcion en url

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const botonTexto = document.getElementById("botonTexto")
 const formularioImagen = document.getElementById("formularioImagen")
 const formularioTexto = document.getElementById("formularioTexto")
 
+// Funcion boton texto-imagen
 botonTexto.onclick = () => {
     formularioImagen.style.display = ("none")
     formularioTexto.style.display = ("block")
@@ -11,3 +12,15 @@ botonImagen.onclick = () => {
     formularioImagen.style.display = ("block")
     formularioTexto.style.display = ("none")
 }
+
+// Funcion URL
+const cambio = () => {
+    const inputTexto  = document.getElementById("inputTexto").value
+    const imagen = document.getElementById ("imagen")
+
+    imagen.removeAttribute("scr")
+    imagen.setAttribute("src",inputTexto)
+
+}
+
+inputTexto.onchange = cambio
