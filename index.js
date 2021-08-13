345cf7c5d98a920ca0e06f55e92b0ca15772a525
Victoria Diaz <diazmariavictoria94@gmail.com>
2021-08-13 20:48:04 -0300

    agrego funcion modo oscuro

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-const botonImagen = document.getElementById("botonImagen")
-const botonTexto = document.getElementById("botonTexto")
-const formularioImagen = document.getElementById("formularioImagen")
-const formularioTexto = document.getElementById("formularioTexto")
+const botonImagen = document.getElementById("botonImagen");
+const botonTexto = document.getElementById("botonTexto");
+const formularioImagen = document.getElementById("formularioImagen");
+const formularioTexto = document.getElementById("formularioTexto");
+const modoOscuro = document.querySelector("#modoOscuro");
+const navModoOscuro = document.querySelector("nav");
+const mainModoOscuro = document.querySelector("main");
+const asideModoOscuro = document.querySelector("aside");
+const botonesNavOscuro = document.getElementsByClassName("boton-nav");
 
 // Funcion boton texto-imagen
 botonTexto.onclick = () => {
-    formularioImagen.style.display = ("none")
-    formularioTexto.style.display = ("block")
-}
+	formularioImagen.style.display = "none";
+	formularioTexto.style.display = "block";
+};
 
 botonImagen.onclick = () => {
-    formularioImagen.style.display = ("block")
-    formularioTexto.style.display = ("none")
-}
+	formularioImagen.style.display = "block";
+	formularioTexto.style.display = "none";
+};
+//funcion modo claro, modo oscuro
+
+modoOscuro.onclick = () => {
+	console.log("cambiar modo");
+	navModoOscuro.classList.remove("nav-modo-claro");
+	navModoOscuro.classList.add("nav-modo-oscuro");
+	console.log("hola");
+	mainModoOscuro.classList.remove("main-modo-claro");
+	mainModoOscuro.classList.add("main-modo-oscuro");
+	asideModoOscuro.classList.remove("aside-modo-claro");
+	asideModoOscuro.classList.add("aside-modo-oscuro");
+	botonesNavOscuro.classList.remove("boton-nav");
+	botonesNavOscuro.classList.add("botones-modo-oscuro");
+};
 
 // Funcion URL
 const cambio = () => {
-    const inputTexto  = document.getElementById("inputTexto").value
-    const imagen = document.getElementById ("imagen")
+	const inputTexto = document.getElementById("inputTexto").value;
+	const imagen = document.getElementById("imagen");
 
-    imagen.removeAttribute("scr")
-    imagen.setAttribute("src",inputTexto)
+	imagen.removeAttribute("src");
+	imagen.setAttribute("src", inputTexto);
+};
 
-}
-
-inputTexto.onchange = cambio
+inputTexto.onchange = cambio;
